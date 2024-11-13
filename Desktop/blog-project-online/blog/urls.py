1ca73e0c619dfeebeb14670a3f2260cb291672bf
rafael <rafaelcamposgranadillo@gmail.com>
2024-11-13 17:19:19 -0400

    create post

--- Desktop/blog-project-online/blog/urls.py
+++ Desktop/blog-project-online/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListview,postDetaIlView
+from .views import PostListview,postDetaIlView,postCreateview
 
 urlpatterns = [
     path("",PostListview.as_view(), name="home"),
     path("post/<int:pk>/", postDetaIlView.as_view(), name="post_detail"),
+    path("post/new/",postCreateview.as_view(), name="post_new"),
 ]
--- Desktop/blog-project-online/blog/views.py
+++ Desktop/blog-project-online/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
-from .models import Post
+from django.views.generic.edit  import CreateView
+from .models import Post 
+
+
 # Create your views here.
 
 class PostListview(ListView):
@@ -10,3 +13,8 @@ class PostListview(ListView):
 class postDetaIlView(DetailView):
     model = Post
     template_name = "post-detail.html"
+    
+class postCreateview(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title","author","body"]
